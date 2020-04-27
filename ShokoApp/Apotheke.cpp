3b30cf8f2793feb16b06e7831d43045fd08f7700
Maria Şuteu <mara.suteu@gmail.com>
2020-04-27 14:30:21 +0300

    apotheke

--- ShokoApp/Apotheke.cpp
+++ ShokoApp/Apotheke.cpp
@@ -3,11 +3,26 @@
 #include <vector>
 using namespace std;
 
-template <class Medikament>
+//template <class Medikament>
 
 class Apotheke {
 private:
-	vector<Medikament> Medlist;
+	//vector<Medikament> Medlist;
 public:
+	Apotheke();
+	~Apotheke();
+	//1
+	void addMed(); //hinzufugen
+	void delMed(); //loschen
+	void aktMed(); //aktualisieren
+	//2
+	void showMed(); //zeigen Med oder alle andere in ABC Reihenfolge
+	//3
+	void knappMed(); //weniger als cin<<x
+	//4
+	void preisMed(); //nach Preis gruppiert
+	//5
+	//undo
+	//redo
 
 };
--- ShokoApp/Apotheke.h
+++ ShokoApp/Apotheke.h
@@ -1,2 +1,30 @@
 #pragma once
+using namespace std;
 
+Apotheke::Apotheke() {
+
+}
+
+Apotheke::~Apotheke() {
+
+};
+
+void Apotheke::addMed() {
+
+};
+
+void Apotheke::delMed() {
+
+};
+
+void Apotheke::aktMed() {
+
+};
+
+void Apotheke::showMed() {
+
+};
+
+void Apotheke::knappMed() {
+
+};
